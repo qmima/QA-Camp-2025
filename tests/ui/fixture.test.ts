316fbeb52a3ce5adc53c2c6c9f794b0231620d22
Alfred Urbanek <alfred.urbanek@inetum.com>
2025-04-07 17:03:55 +0200

    added log

--- tests/ui/fixture.test.ts
+++ tests/ui/fixture.test.ts
@@ -1,8 +1,9 @@
 import { test } from '../fixture';
 
-test('has title', async ({ playwrightPage }) => {
+test('has title', async ({ playwrightPage, browserName }) => {
   await playwrightPage.goto();
   await playwrightPage.toHaveTitle();
+  console.log(browserName);
 });
 
 test('get started link', async ({ playwrightPage }) => {
