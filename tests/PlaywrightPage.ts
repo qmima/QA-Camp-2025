8a6c5da94d2cd18e5553a476dae4ae298f67096d
Alfred <alfredu@gazeta.pl>
2025-04-25 11:35:26 +0200

    added tests using fixture
    Co-authored-by: Alfred Urbanek <alfred.urbanek@inetum.com>

---
+++ tests/PlaywrightPage.ts
@@ -0,0 +1,21 @@
+import { Page, expect } from '@playwright/test';
+
+export class PlaywrightPage {
+    constructor(private page: Page) {}
+
+    async goto() {
+        await this.page.goto('https://playwright.dev/');
+    }
+
+    async toHaveTitle() {
+        await expect(this.page).toHaveTitle(/Playwright/);
+    }
+
+    async clickLink() {
+        await this.page.getByRole('link', { name: 'Get started' }).click();
+    }
+
+    async toHaveHeading() {
+        await expect(this.page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    }
+}
