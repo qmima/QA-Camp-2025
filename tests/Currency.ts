c86d14b9e406d7043d4c5388798696f9a640664c
Alfred Urbanek <alfred.urbanek@inetum.com>
2025-05-13 23:43:18 +0200

    added Currency class

---
+++ tests/Currency.ts
@@ -0,0 +1,25 @@
+class Currency {
+  private static instance: Currency;
+  private actualCurrency: string;
+
+  protected constructor() {
+    this.actualCurrency = '$';
+  }
+
+  public static getInstance(): Currency {
+    if (!Currency.instance) {
+      Currency.instance = new Currency();
+    }
+    return Currency.instance;
+  }
+
+  public changeCurrency(newCurrency: string) {
+    this.actualCurrency = newCurrency;
+  }
+
+  public getCurrency() {
+    return this.actualCurrency;
+  }
+}
+
+export const currency = Currency.getInstance();
---
+++ tests/e2e/currencies.test.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@playwright/test';
+import { currency } from '../Currency';
+
+test('currency change and verification', async ({ page }) => {
+  const curriences = [
+    { name: 'Euro', symbol: '€' },
+    { name: 'US Dollar', symbol: '$' },
+    { name: 'Pound Sterling', symbol: '£' },
+  ];
+
+  await page.goto('https://automationteststore.com/');
+
+  // let menu = await page.locator('ul.language');
+  // await menu.hover();
+
+  // let option = await page.locator('ul.currency li').nth(0);
+  // await option.click();
+
+  // let symbol = await page.textContent('a.dropdown-toggle span.label');
+  // await expect(symbol).toBe('€');
+
+  for (let i = 0; i < curriences.length; ++i) {
+    let item = curriences[i];
+
+    let currencyMenu = page.locator('ul.language');
+    await currencyMenu.hover();
+
+    let option = page
+      .getByRole('listitem')
+      .filter({ hasText: item.symbol + ' ' + item.name })
+      .nth(1);
+    await option.click();
+    currency.changeCurrency(item.symbol);
+
+    let symbol = await page.textContent('a.dropdown-toggle span.label');
+    expect(symbol).toBe(currency.getCurrency());
+  }
+});
