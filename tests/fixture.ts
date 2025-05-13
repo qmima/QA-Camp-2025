cc6326c4504a1a2e72b101322bbcbf545e68b244
Alfred Urbanek <alfred.urbanek@inetum.com>
2025-05-13 22:59:41 +0200

    removed not required files

--- tests/fixture.ts
+++
@@ -1,9 +0,0 @@
-import { test as base } from '@playwright/test';
-import { PlaywrightPage } from './PlaywrightPage';
-
-export const test = base.extend<{ playwrightPage: PlaywrightPage }>({
-    playwrightPage: async ({ page }, use) => {
-        const playwrightPage = new PlaywrightPage(page);
-        await use(playwrightPage);
-    }
-});
