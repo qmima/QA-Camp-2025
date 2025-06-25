a30a7855e00560bd9d86b46dfbf2ebc84fb2805a
Mikołaj Marzec <Mikolaj.Marzec@skanska.pl>
2025-06-25 21:38:05 +0200

    add faker js

--- page-object-model/constants/user.ts
+++ page-object-model/constants/user.ts
@@ -7,15 +7,44 @@ interface User {
   password: string;
 }
 
-class UserBuilder implements User {
-  private user: User;
-}
+export class UserBuilder implements User {
+  firstName: string;
+  lastName: string;
+  email: string;
+  adress: string;
+  city: string;
+  password: string;
 
-class uUserBuilder implements User {
-  private user;
-
-  setFirstName(firstName: string): uUserBuilder {
-    this.user.firstname = firstName;
+  setFirstName(firstName: string) {
+    this.firstName = firstName;
     return this;
+  }
+
+  setLastName(lastName: string) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  setEmail(email: string) {
+    this.email = email;
+    return this;
+  }
+
+  setAdress(adress: string) {
+    this.adress = adress;
+    return this;
+  }
+
+  setCity(city: string) {
+    this.city = city;
+    return this;
+  }
+
+  createNewUser(builder: UserBuilder) {
+    builder.setFirstName('1');
+    builder.setLastName('1');
+    builder.setEmail('1');
+    builder.setAdress('1');
+    builder.setCity('1');
   }
 }
