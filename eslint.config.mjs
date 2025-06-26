d9994ffe2b4a2feda1ed51cb0fb5ef6bdb26b4ff
Alfred Urbanek <alfred.urbanek@inetum.com>
2025-06-26 15:53:24 +0200

    tools installation

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  tseslint.configs.recommended,
+	{
+		ignores: ["node_modules/", "test-results/", "playwright-report", "summary.json", ".vscode/*", ".DS_Store", "Thumbs.db", "*_spec3.json"],
+	},
+]);
