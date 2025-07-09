c589bc37dede9d77badcaae1d75c98c7354633ac
mima-design <mikolaj.marzec@o2.pl>
2025-07-09 15:04:26 +0200

    locators changes

--- tests/e2e/logInPage.spec.ts
+++ tests/e2e/logInPage.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '../../page-object-model/fixtures/automation-test.fixture';
 import { UserBuilder, UserDirector } from '../../page-object-model/constants/user';
+import { log } from 'console';
 
 const builder = new UserBuilder();
 const director = new UserDirector(builder);
@@ -15,7 +16,7 @@ test('1. Positive user registration', async ({ homePage }) => {
   await logInPage.toHaveTitle('Create Account');
 
   //fill User data
-  await logInPage.fillFirstName(user.firstName);
+  await logInPage.interactwith('input', 'AccountFrm_firstname', user.firstName);
   await logInPage.fillLastName(user.lastName);
   await logInPage.fillEmail(user.email);
   await logInPage.fillTelephone(user.telephone);
@@ -24,10 +25,16 @@ test('1. Positive user registration', async ({ homePage }) => {
   await logInPage.fillAdress1(user.adress1);
   await logInPage.fillAdress2(user.adress2);
   await logInPage.fillCity(user.city);
-  await logInPage.selectRegion('West Yorkshire');
   await logInPage.fillZipCode(user.zipCode);
-  await logInPage.selectCountry('United Kingdom');
+  await logInPage.selectCountry('Taiwan');
+  await logInPage.selectRegion('Chia-i');
   await logInPage.fillUserName(user.username);
   await logInPage.fillPassword(user.password);
   await logInPage.fillPasswordConfirm(user.password);
+
+  await logInPage.interactwith('checkbox', 'AccountFrm_agree');
+  await logInPage.interactwith('checkbox', 'AccountFrm_newsletter0');
+
+  await logInPage.clickRegisterButton();
+  await logInPage.conirmationMessage();
 });
