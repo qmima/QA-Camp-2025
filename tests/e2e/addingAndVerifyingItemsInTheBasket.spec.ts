9b214b0d16ac4b545e6e652ea3e17b315e90e28a
AgiSokolowska <agi.sokolowska@gmail.com>
2025-09-17 14:54:04 +0200

    small fix

--- tests/e2e/addingAndVerifyingItemsInTheBasket.spec.ts
+++ tests/e2e/addingAndVerifyingItemsInTheBasket.spec.ts
@@ -1,26 +1,36 @@
-import { ProductsSections } from '../../page-object-model/constants/products-sections';
-import { HomePageProductsSection } from '../../page-object-model/steps/home-page-products-section';
 import { expect } from '@playwright/test';
-import { ProductPageCard } from '../../page-object-model/steps/product-page-card';
 import { test } from '../../page-object-model/fixtures/automation-test.fixture';
+import { Product } from '../../page-object-model/constants/product';
+import { HomePageProductsSection } from '../../page-object-model/steps/home-page-products-section';
+import { ProductPageCard } from '../../page-object-model/steps/product-page-card';
+import { HoverProductCard } from '../../page-object-model/steps/hover-product-card';
+import { ProductData } from '../../page-object-model/constants/product-data';
+import { ProductsSections } from '../../page-object-model/constants/products-sections';
 
-test('adding item to basket', async ({ page, homePage }) => {
-  
+test('adding and verifying items in the basket', async ({ page, homePage }) => {
   await homePage.open();
 
    const homePageProductsSection = new HomePageProductsSection(page);
    const featuredSectionProducts = await homePageProductsSection.getProductsFromSection(ProductsSections.featured);
    const productsDetails = await featuredSectionProducts.getProductDetails('Tropiques Minerale Loose Bronzer');
-   
-   expect(productsDetails.imageHref).toEqual('//automationteststore.com/image/thumbnails/18/6f/demo_product03_jpg-100094-250x250.jpg');
-   expect(productsDetails.price).toEqual('$38.50');
 
-   const productPageCard = new ProductPageCard(page);
-   await productPageCard.addProductToBasket('Add to Cart');
+   const expectedProduct = new Product(
+    ProductData.imageHref,
+    ProductData.price,
+    ProductData.title
+  );
 
-    // Add the product to the basket
-       
-    // Verify that the product was added to the basket
-    //const basketItems = await productPageCard.getBasketItems();
-  
+  expect(productsDetails.imageHref).toEqual(expectedProduct.getImageHref());
+  expect(productsDetails.price).toEqual(expectedProduct.getPrice());
+
+  const productPageCard = new ProductPageCard(page);
+  await productPageCard.addProductToBasket('Add to Cart');
+
+  // Verify that the product was added to the basket
+  const hoverProductCard = new HoverProductCard(page);
+  const basketItems = await hoverProductCard.getHoverProductCard();
+
+  expect(basketItems.imageHref).toEqual(expectedProduct.getImageHref());
+  expect(basketItems.price).toEqual(expectedProduct.getPrice());
+  expect(basketItems.title).toEqual(expectedProduct.getTitle());
 });
