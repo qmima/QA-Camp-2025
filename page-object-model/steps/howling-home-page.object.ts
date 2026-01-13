bbd79a803bd49eb1c032ac50dad582a63772165d
Anna Budnikowska <aniax@poczta.onet.pl>
2026-01-13 15:35:03 +0100

    fixes after review

--- page-object-model/steps/howling-home-page.object.ts
+++ page-object-model/steps/howling-home-page.object.ts
@@ -8,36 +8,37 @@ export class HomePageObject extends BasePageObject {
     await this.toHaveTitle('party - Howling Testers');
   }
 
-
-   async clickAcceptButton() {
+  async clickAcceptButton() {
     await this.page.getByRole('button', { name: 'Accept All' }).click();
   }
-  
-    async closePage() {
+
+  async closePage() {
     await this.page.close();
   }
-    async fillName(name: string) {
+
+  async fillName(name: string) {
     await this.page.getByRole('textbox', { name: 'Imię postaci:' }).fill(name);
   }
 
-    async chooseRole(role: string) {
-   await this.page.locator('#character-creator').getByRole('img', { name: role }).click();
+  async chooseRole(role: string) {
+    await this.page.locator('#character-creator').getByRole('img', { name: role }).click();
   }
 
-    async fillPowers(powers: string[]) {
+  async fillPowers(powers: string[]) {
     await this.page.getByRole('spinbutton', { name: 'Siła:' }).fill(powers[0]);
     await this.page.getByRole('spinbutton', { name: 'Spryt:' }).fill(powers[1]);
     await this.page.getByRole('spinbutton', { name: 'Energia:' }).fill(powers[2]);
     await this.page.getByRole('spinbutton', { name: 'Zdrowie:' }).fill(powers[3]);
   }
 
-    async chceckPopup() {
+  async chceckPopup() {
     await expect(this.page.locator('#popup-title')).toContainText('Duplikat imienia');
-    await expect(this.page.locator('#popup-message')).toContainText('Postać o tym imieniu już istnieje! Wybierz inne imię.');
-     }
-
-    async addCharacter() {
-    await this.page.getByRole('button', { name: 'Dodaj postać' }).click();
+    await expect(this.page.locator('#popup-message')).toContainText(
+      'Postać o tym imieniu już istnieje! Wybierz inne imię.'
+    );
   }
 
+  async addCharacter() {
+    await this.page.getByRole('button', { name: 'Dodaj postać' }).click();
+  }
 }
