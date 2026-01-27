8637a3cb5e8afa7db40f5dffc310408edfdd9ac9
Anna Budnikowska <aniax@poczta.onet.pl>
2026-01-27 15:26:56 +0100

    Ania walking around website (#22)
    * first steps
    
    * Checking menu labels
    
    * blank lines removed
    
    * formatting changes
    
    * changes after PR
    
    * fixes after CR
    
    * fixes after eslint run
    
    * fix
    
    * CR fixes
    
    * file renamed
    
    * files location changed
    
    * fixes after merge
    
    * one more change

---
+++ page-object-model/steps/menu/main-menu-element-with-icon.ts
@@ -0,0 +1,11 @@
+import { MenuElementInterface } from "./menu-element-interface";
+import { MenuElementWithIcon } from "./menu-element-with-icon";
+
+export class MainMenuElementWithIcon implements MenuElementInterface{
+    
+    constructor (public name: string, public sublist?: MenuElementWithIcon[]) 
+    {
+        
+    }
+    subMenuXpath: string = '/div/ul[1]/li';
+}
---
+++ tests/e2e/menu.spec.ts
@@ -0,0 +1,10 @@
+import { test } from '../../page-object-model/fixtures/automation-test.fixture';
+import { listMenu } from '../../page-object-model/steps/menu-elements';
+
+test('Check menu recursively', async ({ menu, homePage}) => {
+  
+  await homePage.open();
+  const xpath = `//*[@id="categorymenu"]/nav/ul/li`;
+  await menu.getMenuDetailsRec(listMenu, xpath);
+
+});
